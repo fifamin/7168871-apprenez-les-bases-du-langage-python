09b011e382bb286a66ad10c64c36438fbbefb8f9
fifamin <jobhouinsou04@gmail.com>
2025-08-11 16:30:57 -0800

    Update main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,7 @@
 # Écrivez votre code ici !
+Nom = "HOUINSOU"
+age = 55
+taille = 1.72
+est_etudiant = True
+print(f" Nom: {Nom} Age: {age} Taille:{taille} Est étudiant: {est_etudiant}")
+print(f" Type Nom: type(Nom) Type Age: type(age) Type Taille:type(taille) Type Est étudiant: type(est_etudiant)")
