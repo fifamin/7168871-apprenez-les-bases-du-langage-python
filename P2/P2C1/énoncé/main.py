27a47579c606a9be355c23c7025393ee851c50bd
fifamin <jobhouinsou04@gmail.com>
2025-08-15 02:46:12 -0800

    Update main.py

--- P2/P2C1/énoncé/main.py
+++ P2/P2C1/énoncé/main.py
@@ -1,1 +1,19 @@
 # Ecrivez votre code ici !
+print("Donnez deux nombres")
+nombre1 = input()
+nombre2 = input()
+#print(f"{nombre1}")
+if nombre1.isnumeric() or nombre2.isnumeric():
+   # print(nombre1)
+    int(nombre1)
+    int(nombre2)
+    #print(nombre1)
+    #print(nombre2)
+else:
+    raise SystemExit("Fin du programm")
+print("Donnez le type d'operation +,-,*,/")
+
+operation = input()
+resultat = nombre1 + operation + nombre2
+
+print(resultat)
